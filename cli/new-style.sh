ae5c83f9fdffd428f2351a8a9e8412ec63a3b55a
Kevin Sheard <kevinsheardg@gmail.com>
2023-05-20 23:09:48 +0200

    added some stuff to new-style command

--- cli/new-style.sh
+++ cli/new-style.sh
@@ -12,3 +12,7 @@ touch ios-app-$2.scss
 touch md-app-$2.scss
 
 echo "<link rel=\"stylesheet\" href=\"/compiled/compiled.css\" />" > index.html
+echo "@import \"theme.scss\";" > ios-app-$2.scss
+echo "@import \"theme.scss\";" > md-app-$2.scss
+
+code index.html ios-app-$2.scss md-app-$2.scss
